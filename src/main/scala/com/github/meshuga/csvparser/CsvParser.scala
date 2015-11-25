d40222873492d2740e1bd814ae159f68c0452822
Patryk Orwat <patrykorwat@gmail.com>
2015-11-25 21:40:08 +0100

    some fun

--- src/main/scala/com/github/meshuga/csvparser/CsvParser.scala
+++ src/main/scala/com/github/meshuga/csvparser/CsvParser.scala
@@ -1,6 +1,24 @@
 package com.github.meshuga.csvparser
 
-class CsvParser(columnSeparator: String) {
-  def parse(input: String): Option[List[CsvLine]] =
-    None
+import scala.io.BufferedSource
+
+class CsvParser(columnSeparator: String, headerPresent: Boolean = true) {
+  def parse(input: BufferedSource): Option[CsvFile] = input.isEmpty match {
+    case false =>
+      val lines: Iterator[String] = input.getLines
+      Some(CsvFile(
+        if (headerPresent) Some(CsvHeader(lines.next().split(columnSeparator).toList)) else None,
+        lines.map(line => CsvLine(line.split(columnSeparator).toList)).toList
+      ))
+    case true => None
+  }
+}
+
+case class CsvLine(lineValues: List[String])
+
+case class CsvHeader(csvHeaders: List[String])
+
+case class CsvFile(header: Option[CsvHeader], lines: List[CsvLine]) {
+  def columnValues(index: Int): List[String] =
+    lines.map(line => line.lineValues(index))
 }
--- src/test/scala/com/github/meshuga/csvparser/CsvParserTests.scala
+++ src/test/scala/com/github/meshuga/csvparser/CsvParserTests.scala
@@ -2,16 +2,39 @@ package com.github.meshuga.csvparser
 
 import org.scalatest.FunSuite
 
+import scala.io.Source
+
 class CsvParserTests extends FunSuite {
 
   test("the returned data is defined") {
-    import scala.io.Source
-    val input = Source.fromFile("src/test/resources/commaSimple.csv").mkString
-
+    val input = Source.fromFile("src/test/resources/commaSimple.csv")
     val sut = new CsvParser(",")
 
-    val maybeLines: Option[List[CsvLine]] = sut.parse(input)
+    val maybeLines = sut.parse(input)
 
     assert(maybeLines.isDefined)
+    assert(maybeLines.get.header.isDefined)
+    assert(maybeLines.get.header.get.csvHeaders == List("a", "b"))
+    assert(maybeLines.get.lines.head.lineValues == List("1", "asd"))
+  }
+
+  test("the returned data have no headers") {
+    val input = Source.fromFile("src/test/resources/commaSimple.csv")
+    val sut = new CsvParser(",", headerPresent = false)
+
+    val maybeLines = sut.parse(input)
+
+    assert(maybeLines.isDefined)
+    assert(maybeLines.get.header.isEmpty)
+    assert(maybeLines.get.lines.size == 2)
+    assert(maybeLines.get.lines.head.lineValues == List("a", "b"))
+  }
+
+  test("values for a column are returned") {
+    val sut = CsvFile(None, List(CsvLine(List("1", "2"))))
+
+    val values = sut.columnValues(1)
+
+    assert(values == List("2"))
   }
 }
